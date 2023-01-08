d6c32403cf40eae54fc8f856f379d4bbfeecca73
shu0411 <shu3205@gmail.com>
2023-01-09 06:00:44 +0900

    04開始

---
+++ C#/04_ABC081B/ABC081B/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC081B
+{
+    class Program
+    {
+        //黒板に N 個の正の整数 A1,...,ANが書かれています．
+        //すぬけ君は，黒板に書かれている整数がすべて偶数であるとき，次の操作を行うことができます．
+        //黒板に書かれている整数すべてを，2 で割ったものに置き換える．
+        //すぬけ君は最大で何回操作を行うことができるかを求めてください．
+        //制約
+        //1≤N≤200
+        //1≤Ai≤10^9
+        //入力
+        //入力は以下の形式で標準入力から与えられる。
+        //N
+        //A1A2... AN
+        //出力
+        //すぬけ君は最大で何回操作を行うことができるかを出力せよ．
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
