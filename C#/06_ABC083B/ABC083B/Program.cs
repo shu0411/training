df334d7d9d65136ecc4abd19344e6c5057f2bd59
shu0411 <shu3205@gmail.com>
2023-01-14 02:05:08 +0900

    06 新規作成（未回答）

---
+++ C#/06_ABC083B/ABC083B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC083B
+{
+    class Program
+    {
+        //1 以上 N 以下の整数のうち、
+        //10 進法での各桁の和が A 以上 B 以下であるものの総和を求めてください。
+        //制約
+        //・1≤N≤10^4
+        //・1≤A≤B≤36
+        //・入力はすべて整数である
+        //入力
+        //入力は以下の形式で標準入力から与えられる。
+        //N A B
+        //出力
+        //1 以上 N 以下の整数のうち、
+        //10 進法での各桁の和が A 以上 B 以下であるものの総和を出力せよ。
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
