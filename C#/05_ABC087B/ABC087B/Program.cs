227169334f935d7d2407151dc95436000527bed8
shu0411 <shu3205@gmail.com>
2023-01-14 02:01:55 +0900

    05 プロジェクト名修正

--- C#/05_ABC087B/ABC087B/Program.cs
+++ C#/05_ABC087B/ABC087B/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05_ABC087B
+namespace ABC087B
 {
     class Program
     {
--- C#/05_ABC087B/ABC087B_2/Program.cs
+++ C#/05_ABC087B/ABC087B_2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05_ABC087B_2
+namespace ABC087B_2
 {
     class Program
     {
