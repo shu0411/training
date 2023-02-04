1aeaf91008fb8daf411a16686fda501620932890
shu0411 <shu3205@gmail.com>
2023-02-04 10:15:12 +0900

    10 新規作成

---
+++ C#/10_ABC049C/ABC049C/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC049C
+{
+    class Program
+    {
+        //英小文字からなる文字列 S が与えられます。 
+        //Tが空文字列である状態から始め、
+        //以下の操作を好きな回数繰り返すことで S=T とすることができるか判定してください。
+        //T の末尾に dream dreamer erase eraser のいずれかを追加する。
+        //制約
+        //1≦∣S∣≦10^5
+        //S は英小文字からなる。
+        //入力
+        //入力は以下の形式で標準入力から与えられる。
+        //S
+        //出力
+        //S=T とすることができる場合 YES を、そうでない場合 NO を出力せよ。
+        static void Main(string[] args)
+        {
+            //入力
+
+            //処理
+
+            //出力
+
+        }
+    }
+}
