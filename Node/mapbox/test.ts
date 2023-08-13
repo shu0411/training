72166fbce3c08f867dfb8603c90fd5472466c4e3
shu0411 <shu3205@gmail.com>
2023-08-13 16:21:35 +0900

    Nodeフォルダ修正　Mapbox最初エラー修正

--- Node/mapbox/test.ts
+++ Node/mapbox/test.ts
@@ -1,1 +1,14 @@
+import http, {IncomingMessage, ServerResponse} from 'http'
+
 console.log("Hello! Node.js × TypeScript");
+
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+    // ここに処理を記述
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>Hello Node HTTP!</h1>');
+    res.end();
+});
+
+const port = 8080;
+server.listen(port);
+console.log('Server listen on port ' + port);
---
+++ Node/mapbox2/main.js
@@ -0,0 +1,8 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FzYW1pbiIsImEiOiJjbGt6ZXNsODgwMmZuM2VvNnQwazd4NGRiIn0.8Pd6lOXnhbpNeI5FVx1Z2A';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [139.7670516, 35.6811673],
+    zoom: 15
+});
