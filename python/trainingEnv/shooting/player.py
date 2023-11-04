ce6ec04a257166cfc2236f0eff7ceb4a3e72c5d6
shu3205 <shu3205@gmail.com>
2023-11-04 19:55:16 +0900

    pygame shooting 自機移動実装

--- python/trainingEnv/shooting/player.py
+++ python/trainingEnv/shooting/player.py
@@ -11,17 +11,26 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((50,50))
         self.image.fill(COLOR_RED)
 
-        #載せる台車
+        #自機を載せる台車
         self.rect = self.image.get_rect(center = (x,y))
 
         #移動
         self.direction = pygame.math.Vector2()
+
+        #移動速度取得
         self.speed = player_speed
     
+    ##表示の更新
+    def update(self):
+        self.input()
+        self.move()
+        print(str(self.direction) + "-" + str(self.rect))
+
+    #入力キー取得
     def input(self):
         key = pygame.key.get_pressed()
 
-        #上下移動
+        #上下キー
         if key[pygame.K_UP]:
             self.direction.y = -1
         elif key[pygame.K_DOWN]:
@@ -29,7 +38,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.y = 0
 
-        #左右移動
+        #左右キー
         if key[pygame.K_LEFT]:
             self.direction.x = -1
         elif key[pygame.K_RIGHT]:
@@ -37,5 +46,47 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
     
-    def update(self):
-        pass
+    #移動処理
+    def move(self):
+        #画面橋チェック（画面端での移動速度修正）
+        self.check_edge_screen()
+
+        #移動速度の平準化
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        #X座標移動
+        self.rect.x += self.direction.x * self.speed
+        #self.check_off_screen("x")
+        #y座標移動
+        self.rect.y += self.direction.y * self.speed
+        #self.check_off_screen("y")
+    
+
+    #画面端チェック
+    #画面端でキーが押されたらそちらの方向への移動をしない
+    ##TODO：うまくいってない→たぶん中途半端なところにいる場合を考慮する必要あり
+    def check_edge_screen(self):
+        if self.rect.left == 0 and self.direction.x < 0:
+            self.direction.x = 0
+        if self.rect.right == screen_width and self.direction.x > 0:
+            self.direction.x = 0
+        if self.rect.top == 0 and self.direction.y < 0:
+            self.direction.y = 0
+        if self.rect.bottom == screen_height and self.direction.y > 0:
+            self.direction.y = 0
+
+    #画面外チェック
+    def check_off_screen(self, vector):
+        if vector == "x":
+            if self.rect.left < 0:
+                self.rect.left = 0
+            if self.rect.right > screen_width:
+                self.rect.right = screen_width
+        if vector == "y":
+            if self.rect.top < 0:
+                self.rect.top = 0
+            if self.rect.bottom > screen_height:
+                self.rect.bottom = screen_height
+        #TODO:画面端で斜め移動しようとしたとき、x成分y成分の移動速度は斜め移動の値のままのため、遅くなっている。
+
