be5d786a09a507657a652989907cc5dcec687bb6
shu0411 <shu3205@gmail.com>
2024-06-29 22:18:30 +0900

    ARC180

---
+++ python/trainingEnv/AtCoder/ARC180/A_2.py
@@ -0,0 +1,47 @@
+import io
+import sys
+
+_INPUT = """\
+100
+ABAABAABABBABAABAABAABABBABBABBABBABBABBABBABBABBABBABBABBABBABBABAABABAABABBABBABABBABAABAABAABAABA
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+#############ここから下をコピペ#############
+import queue
+
+#入力
+N = int(input())
+S = input()
+
+#処理
+q1 = queue.LifoQueue()
+set_S = set()
+
+i = 0
+q1.put([S,0])
+set_S.add(S)
+
+while not q1.empty():
+    S,i = q1.get()
+    if len(S) <= 2 or i >= len(S) - 2:
+        continue
+    if S[i:i+3] == "ABA":
+        after_S = S[:i] + "A" + S[i+3:]
+        q1.put([S,i+1])
+        q1.put([after_S, i])
+        set_S.add(after_S)
+    elif S[i:i+3] == "BAB":
+        after_S = S[:i] + "B" + S[i+3:]
+        q1.put([S,i+1])
+        q1.put([after_S, i])
+        set_S.add(after_S)
+    else:
+        q1.put([S,i+1])
+
+out = len(set_S)
+
+#出力
+print(out % (10**9 + 7))
+
+#TLE
