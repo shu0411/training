e33d90c454a375ffddde4823df26560f85b7403b
shu0411 <shu3205@gmail.com>
2024-06-30 12:18:45 +0900

    private_diary C11-7途中

--- python/DjangoEnv/private_diary/diary/models.py
+++ python/DjangoEnv/private_diary/diary/models.py
@@ -1,9 +1,11 @@
+from accounts.models import CustomUser
 from django.db import models
 
 # Create your models here.
 class Diary(models.Model):
-    title = models.CharField('タイトル', max_length=50)
-    content = models.TextField('本文')
+    user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
+    title = models.CharField('タイトル', max_length=40)
+    content = models.TextField('本文', blank=True, null=True)
     photo1 = models.ImageField('写真1', blank=True, null=True)
     photo2 = models.ImageField('写真2', blank=True, null=True)
     photo3 = models.ImageField('写真3', blank=True, null=True)
