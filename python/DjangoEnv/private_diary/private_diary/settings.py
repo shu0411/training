e879091135892e6e755f7f472b80d6da9295df6a
shu0411 <shu3205@gmail.com>
2024-07-28 20:35:03 +0900

    private_diary C12-18 最後の動作確認できず。ログを見直す。

--- python/DjangoEnv/private_diary/private_diary/settings.py
+++ python/DjangoEnv/private_diary/private_diary/settings.py
@@ -36,7 +36,7 @@ LOGGING = {
     },
     # ハンドラの設定
     'handlers': {
-        'console': {
+        'file': {
             'level': 'INFO',
             'class': 'logging.handlers.TimedRotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/django.log'),
