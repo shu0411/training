e98e1ccce11f951f285d97a8602a5f53fadfc181
min__96 <shu3205@gmail.com>
2025-11-18 23:54:03 +0900

    Godotチュートリアル途中（Main完成）

--- Godot/first-game/Main.cs
+++ Godot/first-game/Main.cs
@@ -1,21 +1,69 @@
 using Godot;
 using System;
+using System.IO;
 
 public partial class Main : Node
 {
 	[Export]
 	public PackedScene MobScene { get; set; }
 
+	private int _score = 0;
+
 	public void GameOver()
 	{
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
+	}
 
+	public void NewGame()
+	{
+		_score = 0;
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+	}
+
+	private void OnMobTimerTimeout()
+	{
+		//Mobシーンをインスタンス化
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		//Mobの出現位置をランダムに設定
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+		mob.Position = mobSpawnLocation.Position;
+
+		//Mobの方向を設定
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2; //開始位置の移動方向から90度右に向ける
+		direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);  //少しランダムにずらす
+		mob.Rotation = direction;
+
+		//Mobの速度を設定
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);       //方向に合わせて速度ベクトルを回転
+
+		//Nodeの子としてモブを追加
+		AddChild(mob);
+
+	}
+
+	private void OnScoreTimerTimeout()
+	{
+		_score++;
+	}
+
+	private void OnStartTimerTimeout()
+	{
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		NewGame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
